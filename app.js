f8e47d6b54c174bbb70eb5678a86d970005023a5
Esteban Dujardin <99099656+EstebanD0406@users.noreply.github.com>
2023-04-17 16:15:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let n = 0;
+
+function render() {
+    const title = React.createElement('h1', {}, 
+    'Hello World ',
+    React.createElement('span', {}, n)
+    );
+
+    ReactDOM.render(title, document.querySelector('#app'));
+}
+
+render();
+
+window.setInterval(() => {
+    n++;
+    render();
+}, 1000)
+
+
+// document.querySelector('#app').innerHTML = '<h1>Hello World</h1>';
